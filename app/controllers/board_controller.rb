1458ba8b29bcc189b8d5e130c4925997042b2966
HisakadoMotoki <sagamihara71@gmail.com>
2020-04-01 22:05:53 +0900

    board

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,4 +2,42 @@ class BoardController < ApplicationController
   def index
   	@boards = Board.all
   end
+
+  def new
+  	@board = Board.new
+  end
+  def create
+    @board = Board.new(board_params)
+    @board.save
+    redirect_to root_path
+  end
+
+
+  def edit
+  end
+  def update
+  	if @board.update(board_params)
+  		flash[:success] = "board was UPDATED"
+  		redirect_to root_path
+  	else
+  		render 'edit'
+  end
+  def destroy
+  	@board.destroy
+  	flash[:danger]  = "board was deleted"
+  	redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+
+  def board_params
+  	params.require(:board).permit(:name)
+  end
+  def set_board
+  	@board = Board.find(params[:id])
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'board#index'
   devise_for :users
  # root to: "home#index"
-  get 'home/index'
+  resources :board
+
 end
